09fb6387e1f3e164558ecde74d4d08c3fb027edc
sergimol <sergimol@ucm.es>
2025-01-25 12:32:45 +0100

    Prefab player

--- Assets/Scripts/Sergio/PlayerController.cs
+++ Assets/Scripts/Sergio/PlayerController.cs
@@ -3,20 +3,20 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D movementParent;
-
     [SerializeField]
     float speed;
+
+    Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        movementParent = GetComponentInParent<Rigidbody2D>();
+        rb = GetComponentInParent<Rigidbody2D>();
     }
 
     public void OnMove(CallbackContext context)
     {
         Vector2 moveValue = context.ReadValue<Vector2>();
-        movementParent.angularVelocity = -moveValue.x * speed * 1000 * Time.deltaTime;
+        rb.angularVelocity = -moveValue.x * speed * 1000 * Time.deltaTime;
     }
 }
