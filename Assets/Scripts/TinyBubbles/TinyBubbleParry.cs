f1ab28bea337b33b0e883e49615bb0a742f993fe
Laura Gomez <lauragbodego@gmail.com>
2025-01-25 19:06:12 +0100

    Tiny bubbles

---
+++ Assets/Scripts/TinyBubbles/TinyBubbleParry.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class TinyBubbleParry : MonoBehaviour
+{
+    [SerializeField]
+    int remainingHits;
+    [SerializeField]
+    Color[] colors = new Color[2];
+    Color spritecolor;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spritecolor = colors[0];
+        gameObject.GetComponent<SpriteRenderer>().color = spritecolor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (remainingHits <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void hit()
+    {
+        spritecolor = colors[1];
+        gameObject.GetComponent<SpriteRenderer>().color = spritecolor;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 6 && spritecolor == colors[1])
+        {
+            collision.GetComponent<PinchoParry>().MainBubbleCollided();
+            Destroy(gameObject);
+        }
+    }
+
+}
