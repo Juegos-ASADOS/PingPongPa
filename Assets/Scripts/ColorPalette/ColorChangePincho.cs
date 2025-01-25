8bad98315d7dd5c52e44c31362f97f2348a0368b
miriam-m-s <81752837+miriam-m-s@users.noreply.github.com>
2025-01-25 20:02:53 +0100

    cambio nombre script
    Co-Authored-By: Eli Todd <81906237+elisatodd@users.noreply.github.com>

--- Assets/Scripts/ColorPalette/ColorChangePincho.cs
+++ Assets/Scripts/ColorPalette/ColorChangePincho.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ColorPalette : MonoBehaviour
+public class ColorChangePincho : MonoBehaviour
 {
     public Color PlayerColor1 =new Color(0.259f,0.863f,0.737f);
     public Color PlayerColor2 =new Color(1.0f,0.631f,0.773f);
